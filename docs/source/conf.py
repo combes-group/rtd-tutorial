b08898450711b3f7db1db35e52ae5e66e282a998
Eli Weissler <eliweissler@gmail.com>
2023-06-14 12:30:43 -0600

    trying rst for table

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     "myst_parser",
-    # "sphinx.ext.mathjax"
-    "sphinx.ext.imgmath"
+    "sphinx.ext.mathjax"
+    # "sphinx.ext.imgmath"
 ]
 
 intersphinx_mapping = {
@@ -38,5 +38,5 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 # -- Options for math output
-imgmath_image_format = 'svg'
-# mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+# imgmath_image_format = 'svg'
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
